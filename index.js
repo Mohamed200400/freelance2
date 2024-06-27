8d52771cb63c63ed34fa50c8e8d5017c84014ada
Mohamed200400 <mohamedamine200400@gmail.com>
2024-06-27 20:34:50 +0200

    third

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const conn = mongoose.Connection;
 app.use(express.json())
 app.use(cors())
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'back/dist')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  res.sendFile(path.join(__dirname + '/back/dist/index.html'));
 });
 
 
