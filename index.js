d53ddc2c93f867eb1d169b9a12e2dbdfcbc7b28c
Mohamed200400 <mohamedamine200400@gmail.com>
2024-07-03 18:23:12 +0200

    last

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const express = require("express")
 const cors = require("cors")
 const user = require("./router/User")
 const question = require("./router/Question")
+require("dotenv").config()
+const nodemailer = require('nodemailer');
 
 
 const app = express()
 
-mongoose.connect("mongodb+srv://mohamed:admin123@project1.sxosfwz.mongodb.net/project1").then(()=>{console.log('db connected')})
+ //mongoose.connect("mongodb+srv://mohamed:admin123@project1.sxosfwz.mongodb.net/project1").then(()=>{console.log('db connected')})
 
- //mongoose.connect("mongodb://0.0.0.0/Question").then(()=>{console.log('db connected');})
+mongoose.connect("mongodb://0.0.0.0/Question").then(()=>{console.log('db connected');})
 const conn = mongoose.Connection;
 
 
@@ -22,6 +24,35 @@ app.use(cors())
 app.use("/api",user)
 
 app.use("/api",question)
+
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+});
+
+app.post('/send-email', (req, res) => {
+    const { to, subject, text } = req.body;
+
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: to,
+        subject: subject,
+        text: text,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).send(error.toString());
+        }
+        res.status(200).send('Email sent: ' + info.response);
+    });
+});
+
+
 
 app.listen(5000,()=>{
     console.log('server running');
